34bbd18fb5f3c752eee1489178d656cc6d06ad44
Noah Herron <noah@NoahsDellLaptop.(none)>
2014-03-07 14:52:01 -0500

    updating testing

--- testing/xmlparse.py
+++ testing/xmlparse.py
@@ -1,14 +1,19 @@
 import xml.etree.ElementTree as et
-from PyQt4 import QtCore, QtGui
+import re
+from datetime import datetime
+from datetime import date
+import time
+
 
 
 class Xmlp():
 
   #initiatlizes the class and prepares an XMLtree for parsing
   def __init__(self):
-    #self.tree = et.parse('FMxml.xml')
-    self.tree = et.parse('./data/FM2xml.xml')
+    self.tree = et.parse('data/realFM.xml')
+    #self.tree = et.parse('FM2xml.xml')
     self.root = self.tree.getroot()
+
 
   #This function will return a list.
   #Inside that list will be one list per record.
@@ -35,25 +40,57 @@ class Xmlp():
       for row in child:
        idv = row.get('MODID')
        if not (idv == None):
-	 for col in row:
-	   for data in col:
-	     rList.append(data.text)	
-	 allfound.append(rList)
-       rList = []
+           for col in row:
+             for data in col:
+               #print data.text
+               if(data.text == None or data.text == '?'):
+                 #print "FOUND ONE"
+                 rList.append("NONE")    
+               else:
+                 rList.append(data.text)
+           allfound.append(rList)
+           rList = []
+
+    #print "allfound is this big "+str(len(allfound))    
+    #for record in allfound:
+      #print record
+    
+    for record in allfound:
+      #print record
+      dateInfo = record[0].split('/')
+      #print "dateInfo record[0] "+record[3]+' '+str(dateInfo)
+      #year day month
+      tempDate = date(int(dateInfo[2]),int(dateInfo[0]),int(dateInfo[1]))#pyDate
+      record[0] = tempDate
+      if (record[1] == 'NONE'):
+     #   print "record[1] == NONE "+record[3]
+        tempDate = date(int(dateInfo[2])+1,int(dateInfo[0]),int(dateInfo[1]))#pyDate
+        record[1] = tempDate
+      else:
+    #    print "record[1] != NONE "+record[3] 
+        dateInfo = record[1].split('/')
+        tempDate = date(int(dateInfo[2]),int(dateInfo[0]),int(dateInfo[1]))#pyDate
+        record[1] = tempDate
+      
 
 #    print "Record Fetch"
-#    for k in allfound:  #Associated Filemaker field:
-#      print k[0]        #open_date
-#      print k[1]        #due_date_actual_delivery
-#      print k[2]        #description
-#      print k[3]        #project_number
-#      print k[4]        #status
-#      print k[5]        #artist_name
-#      print k[6]        #editor_name
-#      print k[7]        #client_contact_name
-#      print "----End of Record"
+    #for k in allfound:  #Associated Filemaker field:
+        #print k
+  #    print k[0]        #open_date
+   #   print k[1]        #due_date_actual_delivery
+    #  print k[2]        #description
+     # print k[3]        #project_number
+      #print k[4]        #status
+      #print k[5]        #artist_name
+      #print k[6]        #editor_name
+      #print k[7]        #client_contact_name
+      #print "----End of Record"
+#    for record in allfound:
+#    	record[0] = #strip that bitch		and record [1]
+
     return allfound
  
+
 
 if __name__ == "__main__":
 
